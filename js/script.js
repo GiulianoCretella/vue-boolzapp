ce59ecf2633492927c9913e0c7b3b38782606ed9
GiulianoCretella <giulianocretella.1@gmail.com>
2022-04-28 15:45:48 +0200

    funzione elimina messaggio

--- js/script.js
+++ js/script.js
@@ -17,17 +17,20 @@ const app = new Vue({
                     {
                         date: '10/01/2020 15:30:55',
                         message: 'Hai portato a spasso il cane?',
-                        status: 'sent'
+                        status: 'sent',
+                        display:'false'
                     },
                     {
                         date: '10/01/2020 15:50:00',
                         message: 'Ricordati di stendere i panni',
-                        status: 'sent'
+                        status: 'sent',
+                        display:'false'
                     },
                     {
                         date: '10/01/2020 16:15:22',
                         message: 'Tutto fatto!',
-                        status: 'received'
+                        status: 'received',
+                        display:'false'
                     }
                 ],
             },
@@ -40,17 +43,20 @@ const app = new Vue({
                     {
                         date: '20/03/2020 16:30:00',
                         message: 'Ciao come stai?',
-                        status: 'sent'
+                        status: 'sent',
+                        display:'false'
                     },
                     {
                         date: '20/03/2020 16:30:55',
                         message: 'Bene grazie! Stasera ci vediamo?',
-                        status: 'received'
+                        status: 'received',
+                        display:'false'
                     },
                     {
                         date: '20/03/2020 16:35:00',
                         message: 'Mi piacerebbe ma devo andare a fare la spesa.',
-                        status: 'sent'
+                        status: 'sent',
+                        display:'false'
                     }
                 ],
             },
@@ -63,17 +69,20 @@ const app = new Vue({
                     {
                         date: '28/03/2020 10:10:40',
                         message: 'La Marianna va in campagna',
-                        status: 'received'
+                        status: 'received',
+                        display:'false'
                     },
                     {
                         date: '28/03/2020 10:20:10',
                         message: 'Sicuro di non aver sbagliato chat?',
-                        status: 'sent'
+                        status: 'sent',
+                        display:'false'
                     },
                     {
                         date: '28/03/2020 16:15:22',
                         message: 'Ah scusa!',
-                        status: 'received'
+                        status: 'received',
+                        display:'false'
                     }
                 ],
             },
@@ -86,12 +95,14 @@ const app = new Vue({
                     {
                         date: '10/01/2020 15:30:55',
                         message: 'Lo sai che ha aperto una nuova pizzeria?',
-                        status: 'sent'
+                        status: 'sent',
+                        display:'false'
                     },
                     {
                         date: '10/01/2020 15:50:00',
                         message: 'Si, ma preferirei andare al cinema',
-                        status: 'received'
+                        status: 'received',
+                        display:'false'
                     }
                 ],
             },
@@ -104,12 +115,14 @@ const app = new Vue({
                     {
                         date: '10/01/2020 15:30:55',
                         message: 'Ricordati di chiamare la nonna',
-                        status: 'sent'
+                        status: 'sent',
+                        display:'false'
                     },
                     {
                         date: '10/01/2020 15:50:00',
                         message: 'Va bene, stasera la sento',
-                        status: 'received'
+                        status: 'received',
+                        display:'false'
                     }
                 ],
             },
@@ -122,17 +135,20 @@ const app = new Vue({
                     {
                         date: '10/01/2020 15:30:55',
                         message: 'Ciao Claudia, hai novità?',
-                        status: 'sent'
+                        status: 'sent',
+                        display:'false'
                     },
                     {
                         date: '10/01/2020 15:50:00',
                         message: 'Non ancora',
-                        status: 'received'
+                        status: 'received',
+                        display:'false'
                     },
                     {
                         date: '10/01/2020 15:51:00',
                         message: 'Nessuna nuova, buona nuova',
-                        status: 'sent'
+                        status: 'sent',
+                        display:'false'
                     }
                 ],
             },
@@ -145,12 +161,14 @@ const app = new Vue({
                     {
                         date: '10/01/2020 15:30:55',
                         message: 'Fai gli auguri a Martina che è il suo compleanno!',
-                        status: 'sent'
+                        status: 'sent',
+                        display:'false',
                     },
                     {
                         date: '10/01/2020 15:50:00',
                         message: 'Grazie per avermelo ricordato, le scrivo subito!',
-                        status: 'received'
+                        status: 'received',
+                        display:'false',
                     }
                 ],
             },
@@ -163,17 +181,20 @@ const app = new Vue({
                     {
                         date: '10/01/2020 15:30:55',
                         message: 'Ciao, andiamo a mangiare la pizza stasera?',
-                        status: 'received'
+                        status: 'received',
+                        display:'false',
                     },
                     {
                         date: '10/01/2020 15:50:00',
                         message: 'No, l\'ho già mangiata ieri, ordiniamo sushi!',
-                        status: 'sent'
+                        status: 'sent',
+                        display:'false'
                     },
                     {
                         date: '10/01/2020 15:51:00',
                         message: 'OK!!',
-                        status: 'received'
+                        status: 'received',
+                        display:'false'
                     }
                 ],
             }
@@ -193,16 +214,19 @@ const app = new Vue({
         },
         addMioMessaggio(){
             const newMessage = {
-                date: '10/01/2020 15:51:00',
+                date: dayjs().format('HH:mm'),
                 message:this.mioMessaggio,
-                status:'sent'
+                status:'sent',
+                display:'false'
             };
             this.contacts[this.currentIndex].messages.push(newMessage);
             this.mioMessaggio='';
+            console.log(newMessage.date)
             const messaggioUtente= {
-                date: '10/01/2020 15:51:00',
+                date: dayjs().format('HH:mm'),
                 message:'OK!',
-                status:'received'
+                status:'received',
+                display:'false'
             }
             setTimeout(()=>{
                 this.contacts[this.currentIndex].messages.push(messaggioUtente);
@@ -210,6 +234,9 @@ const app = new Vue({
         },
         eliminaMessaggio(index){
             this.contacts[this.currentIndex].messages.splice(index,1)
+        },
+        showRemove: function(index,currentIndex){
+           this.contacts[currentIndex].messages[index].display = !this.contacts[currentIndex].messages[index].display
         }
     },
     computed:{
@@ -233,3 +260,10 @@ const app = new Vue({
 
 // Milestone 4
 // ●	Ricerca utenti: scrivendo qualcosa nell’input a sinistra, vengono visualizzati solo i contatti il cui nome contiene le lettere inserite (es, Marco, Matteo Martina -> Scrivo “mar” rimangono solo Marco e Martina)
+
+
+//Milestone 5
+// ●	Cancella messaggio: cliccando sul messaggio appare un menu a tendina che permette di cancellare il messaggio selezionato
+
+// ●	Visualizzazione ora e ultimo messaggio inviato/ricevuto nella lista dei contatti 
+
